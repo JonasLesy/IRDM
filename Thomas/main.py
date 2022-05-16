03ad890f705380e3057c10ece33846fffeb38486
Bytebier <thomas.bytebier@telenet.be>
2022-05-16 22:20:00 +0200

    Start taak 3 (pair programing)

--- Thomas/main.py
+++ Thomas/main.py
@@ -7,7 +7,8 @@ import collections, functools, operator
 import datetime as dt
 import numpy as np
 import random as r
-from scipy.sparse import csr_matrix, lil_matrix
+from scipy.sparse import csr_matrix, csc_matrix, lil_matrix, diags
+from scipy.sparse import linalg
 
 
 # Parse an input file.
@@ -40,7 +41,7 @@ def create_sparse_matrices(movies, users, ratings):
     users_array = np.asarray(users)
     ratings_array = np.asarray(ratings)
 
-    movies_x_users = csr_matrix((ratings_array, (movies_array, users_array)))
+    movies_x_users = csr_matrix((ratings_array, (movies_array, users_array)), dtype=float)
 
     # TODO: transpose should be better than performing the create_matrix function(?)
     users_x_movies = movies_x_users.transpose()
@@ -81,7 +82,7 @@ def dimsum_mapper(matrix, norms, rows, gamma):
     return map(m, rows)
 
 
-def dimsum_reducer(emissions, nr_of_movies):
+def dimsum_reducer(emissions, norms, nr_of_movies):
     reducer_result = functools.reduce(operator.add, emissions)
 
     #b_matrix = np.zeros(shape = (nr_of_movies, nr_of_movies))
@@ -141,6 +142,90 @@ def get_and_print_time(msg, prev):
         return now
 
 
+
+
+
+
+
+
+def taak3(matrix):
+    # Summarize dataset with SVD.
+    k = matrix.shape[0] - 1
+    print('k=' + str(k))
+    q_matrix, s, vt = linalg.svds(matrix, k = k)
+
+    # From SVD we can calculate the matrices Q and P, which are needed for the SGD algorithm.
+    pt_matrix = diags(s) @ vt
+    # p_matrix = pt_matrix.transpose()
+
+    # print(np.asarray(matrix))
+    # print(matrix.toarray())
+    nonzero_rows, nonzero_cols = matrix.nonzero()
+
+
+    unique_rows = np.unique(nonzero_rows)
+
+    # print()
+    # print(matrix[1,:])
+    # print()
+
+    hyperparam_1 = 1
+    print()
+    print(q_matrix)
+    print()
+
+    gradient_step = 0.00001
+    for a in range(1):
+        for x in unique_rows:
+            row = matrix.getrow(x)
+            cols = row.indices
+            
+            nabla_q = 0
+            
+            for i in cols:
+                for f in range(k):
+                    A = matrix[x,i]
+                    B = q_matrix[x,f]
+                    C = pt_matrix[f,i]
+
+                    nabla_q += -2 * (A - B*C) * C + 2*hyperparam_1*B
+                    #print(yyy)
+
+            correction_curr_row = gradient_step * nabla_q
+
+            for f in range(k):
+                q_matrix[x,f] = q_matrix[x,f] - correction_curr_row
+
+
+                # print(i, x, matrix[i,x])
+                # xxx = 2 * (matrix[i,x] - matrix[i,:] @ matrix[:,x])
+
+    print(q_matrix)
+    print()    
+
+    
+    # A = matrix[x,i]
+    # B = Q[x,f]
+    # C = P[f,i]
+    # yyy = 2 * (A - B*C) * C + 2.lambda*B
+
+
+    # print(q_matrix)
+    # print()
+    # print(pt_matrix)
+    # print()
+    # result = q_matrix @ diags(s) @ vt
+    # print (result)
+
+
+
+
+
+    
+
+
+
+
 # Run
 movies, users, ratings = [], [], []
 mac = "/Users/thomasbytebier/Documents/School/IRDM/Project/netflix_dataset/"
@@ -149,11 +234,10 @@ folder = windows
 gamma = 100
 
 print()
-
 t1_start = get_and_print_time("Task 1", None)
 #parse_file(folder + "smallest.txt", movies, users, ratings)
-parse_file(folder + "small.txt", movies, users, ratings)
-#parse_file(folder + "medium.txt", movies, users, ratings)
+#parse_file(folder + "small.txt", movies, users, ratings)
+parse_file(folder + "medium.txt", movies, users, ratings)
 #parse_file(folder + "combined_data_1.txt", movies, users, ratings)
 #parse_file(folder + "combined_data_2.txt", movies, users, ratings)
 #parse_file(folder + "combined_data_3.txt", movies, users, ratings)
@@ -163,24 +247,29 @@ movies_x_users, users_x_movies = create_sparse_matrices(movies, users, ratings)
 t1_sparse = get_and_print_time("Finished creating sparse matrices", t1_read)
 t1_finish = get_and_print_time("Finished task 1", t1_start)
 
-print()
+# print()
+# t2_start = get_and_print_time("Task 2", None)
+# actual_atraspose_a = calculate_atranspose_a(movies_x_users, users_x_movies)
+# nonzero_rows, nonzero_cols = users_x_movies.nonzero()
+# unq_nonzero_rows = np.unique(nonzero_rows)
+# unq_nonzero_cols = np.unique(nonzero_cols)
+# norms = calculate_vector_norm(users_x_movies, unq_nonzero_cols)
+# t2_norms = get_and_print_time("Finished creating norms", t2_start)
+# map_result = dimsum_mapper(users_x_movies, norms, unq_nonzero_rows, gamma)
+# t2_mapper = get_and_print_time("Finished mapper", t2_norms)
+# b_matrix = dimsum_reducer(map_result, norms, len(movies) + 1)
+# t2_reducer = get_and_print_time("Finished reducer", t2_mapper)
+# approx_atraspose_a = approximate_atranspose_a(b_matrix, norms, len(movies) + 1)
+# t2_approximation = get_and_print_time("Finished approximation", t2_reducer)
+# compare_atranspose_a(actual_atraspose_a, approx_atraspose_a)
+# t2_compare = get_and_print_time("Finished comparison", t2_approximation)
+# t2_finish = get_and_print_time("Finished task 2", t2_start)
 
-t2_start = get_and_print_time("Task 2", None)
-actual_atraspose_a = calculate_atranspose_a(movies_x_users, users_x_movies)
-nonzero_rows, nonzero_cols = users_x_movies.nonzero()
-unq_nonzero_rows = np.unique(nonzero_rows)
-unq_nonzero_cols = np.unique(nonzero_cols)
-norms = calculate_vector_norm(users_x_movies, unq_nonzero_cols)
-t2_norms = get_and_print_time("Finished creating norms", t2_start)
-map_result = dimsum_mapper(users_x_movies, norms, unq_nonzero_rows, gamma)
-t2_mapper = get_and_print_time("Finished mapper", t2_norms)
-b_matrix = dimsum_reducer(map_result, len(movies) + 1)
-t2_reducer = get_and_print_time("Finished reducer", t2_mapper)
-approx_atraspose_a = approximate_atranspose_a(b_matrix, norms, len(movies) + 1)
-t2_approximation = get_and_print_time("Finished approximation", t2_reducer)
-compare_atranspose_a(actual_atraspose_a, approx_atraspose_a)
-t2_compare = get_and_print_time("Finished comparison", t2_approximation)
-t2_finish = get_and_print_time("Finished task 2", t2_start)
-
 print()
+t3_start = get_and_print_time("Task 3", None)
+taak3(movies_x_users)
+
+
+t3_finish = get_and_print_time("Finished task 3", t3_start)
+
 get_and_print_time("Finished project", t1_start)
